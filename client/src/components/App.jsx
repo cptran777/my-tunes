f0b012bc10a60de1fb50563d0eb66d8c908cd409
cptran777 <cptran14@gmail.com>
2016-12-04 22:31:31 -0800

    add app component basic structure

---
+++ client/src/components/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class App extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+    }
+  }
+
+  render() {
+    return (
+      <div id="app-container">
+      </div>
+    );
+  }
+}
+
+export default App;
