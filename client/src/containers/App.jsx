98882000cd107f83ecaef8899702de1cee701af6
cptran777 <cptran14@gmail.com>
2016-12-05 20:04:40 -0800

    rename to camel case. Init our basic app container

--- client/src/containers/App.jsx
+++ client/src/containers/App.jsx
@@ -1,16 +1,44 @@
+// App is the top-most level of the application. 
+// Visual structure of the application is:
+// ----------------------------------------------------------
+//                    BANNER
+// ----------------------------------------------------------
+// 
+//             APP BODY OR LOGIN SCREEN
+// 
+// 
+// ----------------------------------------------------------
+//                    FOOTER
+// ----------------------------------------------------------
+
+/****** IMPORT DEPENDENCIES AND HIGH-LEVEL CONTAINERS ******/
+
 import React from 'react';
 
+import Banner from './Banner';
+import AppBody from './AppBody';
+import LoginBody from './LoginBody';
+import Footer from './Footer';
+
+/********************** MAIN OBJECT ************************/
 class App extends React.Component {
 
   constructor() {
     super();
     this.state = {
+      // Temporary storage of the loggedin state
+      // Should implement some kind of session since putting
+      // it directly here is pretty insecure 
+      loggedIn: false
     }
   }
 
   render() {
     return (
       <div id="app-container">
+        <Banner />
+        {this.state.loggedIn ? AppBody : LoginBody}
+        <Footer />
       </div>
     );
   }
